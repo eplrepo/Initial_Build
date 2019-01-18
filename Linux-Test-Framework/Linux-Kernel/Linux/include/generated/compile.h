36da3fa34eaebcfc44d0357291bf2aa54dd57eb0
purushotham447 <srinivas_r@hcl.com>
2019-01-18 12:35:12 +0530

    Initial commit

---
+++ Linux-Test-Framework/Linux-Kernel/Linux/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 14 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#14 SMP Thu Jan 17 18:31:39 IST 2019"
+#define LINUX_COMPILE_BY "epl"
+#define LINUX_COMPILE_HOST "ERS-EPL-DS-E06995E6B25F"
+#define LINUX_COMPILER "gcc version 6.4.0 (Buildroot 2018.02.1-g1c99f2c)"
